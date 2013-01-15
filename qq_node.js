c0117f3f88e08a6a96e4ea962bddce991d2b9887
Ailocodjam <lgmcolin@gmail.com>
2013-01-15 20:15:51 +0800

    first commit

---
+++ qq_node.js
@@ -0,0 +1,312 @@
+
+var qqinfo ={
+    loginType:2,
+    qq:*****,
+    pwd:*****
+};
+
+var http = require('http');
+var url = require('url');
+var qs = require('querystring');
+var fs = require('fs');
+var rl = require('readline');
+
+//send request ,and return response  
+function post(pUrl,headers,body,onResponse){
+   var args = url.parse(pUrl); 
+  // console.log('args='+args);
+   var options = args;
+   if(headers) options.headers = headers;
+   options.method = 'post';
+   if(!body) options.method = 'get';
+   //console.log('options='+options);
+
+   var req = http.request(options,function(res){
+        if(res.statusCode!=200){
+            onResponse(res); 
+            return;
+        }
+        var body = new Buffer(1024*10);
+        var size = 0;
+        res.on('data',function(chunk){
+            size +=chunk.length;
+            if(size>body.length){
+                var ex = Math.ceil(size/(1024*10)); 
+                var tmp = new Buffer(ex*1024*10);
+                body.copy(tmp);
+                body = tmp;
+            }
+            chunk.copy(body,size - chunk.length);
+        });
+        res.on('end',function(){
+            res.body = new Buffer(size); 
+            body.copy(res.body);
+            //console.log('body='+body);
+            onResponse(res);
+        });
+   }).on('error',function(e){
+        console.log("Got error" + e.message); 
+   }); if(body){
+        req.end(qs.stringify(body)); 
+    }else{
+        req.end(); 
+    }
+}
+//turn to post
+function get(pUrl,headers,onResponse){
+    post(pUrl,headers,null,onResponse);
+}
+
+var userAgend ='Mozilla/5.0 (X11; Linux i686) AppleWebKit/536.11 (KHTML, like Gecko) Ubuntu/12.04 Chromium/20.0.1132.47 Chrome/20.0.1132.47 Safari/536.11';
+var PostHead = {'User-Agent' : userAgend , 'Content-Type' : 'application/x-www-form-urlencoded'}; 
+
+console.log('正在登录qq:'+qqinfo.qq);
+//begin to exec,and set the session_id to Vdata.id
+post('http://pt.3g.qq.com/psw3gqqLogin',PostHead,qqinfo,function(res){
+    if(res.statusCode == 302){
+        var regex = new RegExp('sid=(.[^&]+)',"ig"); 
+        regex.exec(res.headers.location);
+        var sid = RegExp.$1;
+        console.log('登录成功'+sid);
+        Vdata.sid = sid;
+        getFriends();        
+        return;
+    }            
+    if(res.body.indexOf('密码错误')>=0){
+        console.log('密码错误'); 
+        return ;
+    }
+    if(res.body.indexOf('验证码')>=0){
+        console.log('需要输入验证码'); 
+        var regex = new RegExp(' src="(http://vc\.gtimg\.com.[^"]*)"','ig');
+        regex.exec(res.body);
+        var imgurl = RegExp.$1;
+        get(imgurl,null,function(res){
+            fs.writeFile('verify.gif',res.body,0,res.body.length,0,function(err){
+                 console.log('请输入目录下图片：verify.gif 的文字：(TODO:未完成！！)');
+            }); 
+        });
+        return;
+    }
+    console.log("登录失败\\n"+res.body);
+});
+
+var Vdata={
+    sid:''
+}
+var Friends = [];
+
+//get friends
+function getFriends(){
+    Friends = [];
+    console.log('正在获取好友列表....');
+
+    var chatmain = 'http://q16.3g.qq.com/g/s?sid=$SID&aid=nqqchatMain&p=$Page';
+    chatmain = chatmain.replace('$SID',Vdata.sid);
+    chatmain = chatmain.replace('$Page',1);
+    get(chatmain,{'User-Agent':userAgend},function(res){
+        getFriendPage(1,function(){
+            console.log('\n在线好友'+Friends.length+'个:\n'); 
+            for(var i=0;i<Friends.length;i++){
+                var qinfo = Friends[i]; 
+                console.log(i+1 + ':'+qinfo.name +'\t\t['+qinfo.qq+']');
+            }
+            settty();    
+        });     
+    });
+}
+
+
+//get friendspage,and exec the 'next' function,output all the online friends
+function getFriendPage(page,next){
+    var chatmain = 'http://q16.3g.qq.com/g/s?sid=$SID&aid=nqqchatMain&p=$Page';
+    chatmain = chatmain.replace('$SID',Vdata.sid);
+    chatmain = chatmain.replace('$Page',page);
+
+    get(chatmain,{'User-Agent':userAgend},function(res){
+            var body = res.body.toString().replace(/\s+/ig,'');
+          //  var regex = /u=(\d+).+?class="name.*?".*?>(.[^<>]*?)<\/span>/ig;
+            var regex = /u=(\d+).+?class="name.*?".*?>(.[^<>]*?)<\/span>/ig;
+            while(regex.exec(body)){
+                var qinfo = {qq:RegExp.$1,name:RegExp.$2.replace(/^ +?/,'')}; 
+                Friends.push(qinfo);
+            }            
+            var regex2 = new RegExp('第(\\d+?)/(\\d+?)页','ig');
+            if(regex2.exec(body)){
+                var cur = parseInt(RegExp.$1); 
+                var max = parseInt(RegExp.$2);
+                console.log('第'+cur+'/'+max+'页');
+                if(cur>=max){
+                    next(); 
+                    return;
+                }
+                getFriendPage(cur+1,next);
+            }else{
+               // console.log('没有下页...');
+                var regex3 = new RegExp('共(\\d+?)页','ig');
+                if(regex3.exec(body)){
+                    var count = parseInt(RegExp.$1);
+              //    console.log('count='+count);
+                    if(page>count){
+                        next(); 
+                        return;
+                    }
+                    getFriendPage(page+1,next);
+                }
+            }
+    });
+}
+
+//获取分组信息
+var groups=[];
+function getGroups(){
+    console.log('正在获取分组信息'); 
+    groups=[];
+    
+    var chatmain = 'http://q32.3g.qq.com/g/s?sid=$SID&aid=nqqGroup';
+    chatmain = chatmain.replace('$SID',Vdata.sid);
+    var userAgend = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/536.11 (KHTML, like Gecko) Ubuntu/12.04 Chromium/20.0.1132.47 Chrome/20.0.1132.47 Safari/536.11';
+    get(chatmain,{'User-Agent':userAgend},function(res){
+        var body = res.body.toString().replace(/\s+/ig,''); 
+        var regex = /id=(\d+).+?><img.+?\/>(.[^<>]*?)<span/ig;
+        var group_id,group_name,group;
+        while(regex.exec(body)){
+            group_id =parseInt(RegExp.$1), 
+            group_name =RegExp.$2;
+            group = {'group_id':group_id,'group_name':group_name}; 
+            groups.push(group);
+        }
+        console.log('分组情况:');
+        for(var i=0;i<groups.length;i++){
+            console.log('group_id='+groups[i].group_id+'  group_name='+groups[i].group_name);
+        }
+        console.log("选择以下操作(查看第几个分组：show +index,返回q or quit)");
+
+    });
+}
+
+var group_mem = [];
+function getGroupsMem(index){
+    group_mem = [];
+    var chatmain = 'http://q16.3g.qq.com/g/s?sid=$SID&aid=nqqGrpF&name=$NAME&id=$ID&gindex=$GINDEX&pid=$Page'; 
+    chatmain = chatmain.replace('$SID',Vdata.sid);
+    chatmain = chatmain.replace('$Page',1); 
+    chatmain = chatmain.replace('$ID',0).replace('$GINDEX',0);
+    var userAgend = 'User-Agent:Mozilla/5.0 (X11; Linux i686) AppleWebKit/536.11 (KHTML, like Gecko) Ubuntu/12.04 Chromium/20.0.1132.47 Chrome/20.0.1132.47 Safari/536.11';
+    get(chatmain,{'User-Agent':userAgend},function(res){
+        getGroupsMemPage(1,index,function(){
+            console.log('分组 '+groups[index].group_name+' 的成员有');        
+            for(var i=0;i<group_mem.length;i++){
+                var qinfo = group_mem[i]; 
+                console.log(i+1 + ':'+qinfo.name + '\t\t['+qinfo.qq+']');
+            } 
+        }) 
+    });
+}
+
+function getGroupsMemPage(page,index,next){
+    var chatmain = 'http://q16.3g.qq.com/g/s?sid=$SID&aid=nqqGrpF&name=$NAME&id=$ID&gindex=$GINDEX&pid=$Page'; 
+    chatmain = chatmain.replace('$SID',Vdata.sid);
+    chatmain = chatmain.replace('$Page',page); 
+    chatmain = chatmain.replace('$ID',groups[index].group_id).replace('$GINDEX',groups[index].group_id);
+    get(chatmain,{'User-Agent':userAgend},function(res) {
+        var body = res.body.toString().replace(/\s+/ig,'');
+        var regex = /u=(\d+).+?class="name.*?".*?>(.[^<>]*?)<\/span>/ig;
+        while(regex.exec(body)){
+            var qinfo = {qq:RegExp.$1,name:RegExp.$2.replace(/^ +?/,'')};
+            group_mem.push(qinfo);
+        }
+        var regex2 = new RegExp('第(\\d+?)/(\\d+?)页','ig');
+        if(regex2.exec(body)) {
+            var cur = parseInt(RegExp.$1);
+            var max = parseInt(RegExp.$2);
+            if(cur>=max){
+                next();
+                return;
+            }
+            getGroupsMemPage(page+1,index,next);
+        }else {
+            next();  
+        }
+    });
+}
+
+//send message ,param is index and msg
+function sendmsg(index,msg){
+    var qinfo = Friends[index-1];
+    var form = {
+        'u':qinfo.qq,
+        'msg':msg,
+        'aid':'发送'    
+    };
+    var purl = 'http://q32.3g.qq.com/g/s?sid=$SID';
+    purl = purl.replace('$SID',Vdata.sid);
+    console.log(purl);
+    console.log(form);
+    post(purl,{'User-Agent':userAgend,'Content-Type':'application/x-www-form-urlencoded'},form,function(res){
+        var body = res.body.toString();
+        if(body.indexOf('重新发送')>=0){
+            console.log('发送失败'); 
+            return ;
+        } 
+       // console.log(body); 
+    });
+    return true;
+}
+
+var tty;
+//control!!
+function settty(){
+    if(tty) return;
+    tty = rl.createInterface(process.stdin,process.stdout,null);
+    var prefix = '>>';
+    tty.on('line',function(line){
+        switch(line.toLowerCase().trim()){
+            case '?': 
+            case 'help':
+                console.log('命令列表：');
+                console.log('q/quit：退出');
+                console.log('g/group:查看分组');
+                console.log('o/online：在线列表');
+                console.log('show +分组index');
+                console.log('send +friend_index +内容');
+                break;
+            case 'o':
+            case 'online':
+                getFriends();
+                break;
+            case 'q':
+            case 'quit':
+                tty.close();
+                process.exit(0);
+                break;
+            case 'g':
+            case 'group':    
+                getGroups();
+                break;
+            default:
+                var regexp = new RegExp('show +(\\d+)','ig');
+                if(regexp.exec(line)){
+                   getGroupsMem(parseInt(RegExp.$1)); 
+                   return;
+                }
+                var regexp = new RegExp('send +(\\d+) +?(.+)','ig');
+                if(regexp.exec(line)){
+                    if(sendmsg(RegExp.$1,RegExp.$2)){
+                        console.log('发送成功');
+                        return; 
+                    } 
+                }
+               console.log('抱歉，我不能理解您的命令 `' + line.trim() + '`');
+          break;
+        } 
+    }).on('close',function(){
+        console.log('再见'); 
+        process.exit(0);
+    });
+    console.log('输入？/help获得命令帮助');
+    tty.setPrompt(prefix,prefix.length);
+    tty.prompt();
+}
+
+
